b943ab1be2ed7a9d1005ebbd5c794626947a1922
Sihui Shen <sihui@Sihuis-MacBook-Pro.local>
2021-05-18 19:31:02 -0500

    wipall in

---
+++ src/Score.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function Score(props) {
+    return (
+        <div>
+            {props.scores.map((scores) => (
+                <li>
+                    Date: {scores.date} <br /> Score: {scores.score}
+                </li>
+            ))}
+        </div>
+    );
+}
+
+export default Score;
---
+++ src/Student.js
@@ -0,0 +1,19 @@
+import Score from "./Score";
+import "./App.css";
+
+function Student(props) {
+    return (
+        <div>
+            {props.students.map((stud) => (
+                <div>
+                    Name: {stud.name}
+                    <br />
+                    Bio: {stud.bio}
+                    <Score scores={stud.scores} />
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Student;
